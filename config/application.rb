d39b5543b8fae91af6333ac196378f4fca0a0407
sudhakar <sudhakardiyya@gmail.com>
2014-09-19 10:22:34 +0530

    app adding

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Goodservice
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.debug = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
